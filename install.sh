0bae6ba80636fdd47bea818e5fcb65c8acd68e5a
Albert Gouws <albert@kilo.gouws.org>
2021-12-10 17:26:24 +1300

    Install Scripts

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ echo "/usr/bin/i3" >> /etc/xrdp/startwm.sh
 # Run stow to set up dotfiles
 echo Running stow
 #STOW_FOLDERS=i3,nvim,bashrc
-STOW_FOLDERS=a
+STOW_FOLDERS=i3,nvim
 pushd $DOTFILES
 echo $(echo $STOW_FOLDERS | sed "s/,/ /g")
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
