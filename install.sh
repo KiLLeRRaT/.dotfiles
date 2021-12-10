f3c9c55ee93b5c0ca7e5aaf9ffd394750dbbb44a
Albert Gouws <albert@kilo.gouws.org>
2021-12-11 07:23:44 +1300

    Install script

--- install.sh
+++ install.sh
@@ -3,15 +3,19 @@ pushd ~
 echo Adding Neovim package repository
 sudo add-apt-repository ppa:neovim-ppa/unstable
 
+
 echo Updating Linux
 sudo apt update
 sudo apt -y upgrade
 
+
 echo Installing git, xrdp, i3, curl, stow
 sudo apt install -y git xrdp i3 curl stow neovim
 
+
 echo Clone dotfiles
 git clone https://github.com/KiLLeRRaT/.dotfiles.git
+
 
 echo Config xrdp to start with i3
 # EDIT /etc/xrdp/startwm.sh
@@ -21,9 +25,14 @@ echo Config xrdp to start with i3
 #/usr/bin/i3
 sudo sed -Ei.bak 's|test -x /etc/X11/Xsession \&\& exec /etc/X11/Xsession|# test -x /etc/X11/Xsession \&\& exec /etc/X11/Xsession|' /etc/xrdp/startwm.sh
 sudo sed -Ei.bak 's|exec /bin/sh /etc/X11/Xsession|# exec /bin/sh /etc/X11/Xsession|' /etc/xrdp/startwm.sh
-#sudo echo "/usr/bin/i3" >> /etc/xrdp/startwm.sh # DOES NOT WORK FOR SUDO.. USE BELOW
-echo "/usr/bin/i3" | sudo tee -a /etc/xrdp/startwm.sh
-# Run stow to set up dotfiles
+if grep -q "/usr/bin/i3" "/etc/xrdp/startwm.sh" ; then
+	# exists
+else
+	# not exist
+	#sudo echo "/usr/bin/i3" >> /etc/xrdp/startwm.sh # DOES NOT WORK FOR SUDO.. USE BELOW
+	echo "/usr/bin/i3" | sudo tee -a /etc/xrdp/startwm.sh
+fi
+
 echo Running stow
 STOW_FOLDERS=i3,nvim,bashrc
 pushd ~/.dotfiles
