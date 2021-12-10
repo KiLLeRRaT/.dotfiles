615ff38de477c07911f49cbdb1b4e27ce7db07c0
Albert Gouws <albert@kilo.gouws.org>
2021-12-10 16:49:57 +1300

    Added bashrc, updated install script

--- install.sh
+++ install.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
-# sudo apt update
-# sudo apt upgrade
+sudo apt update
+sudo apt upgrade
 
 # Install i3
 # Install curl
 # Install xrdp
 # Install stow
-# sudo apt install xrdp i3 curl stow
+sudo apt install git xrdp i3 curl stow
 
 # Install Neovim
-# sudo add-apt-repository ppa:neovim-ppa/unstable
-# sudo apt update
-# sudo apt install neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
 
 # CONFIG xrdp to start with i3
 # EDIT /etc/xrdp/startwm.sh
-# REMOVE THIS LINE
+# COMMENT OUT THIS LINE
 #exec /bin/sh /etc/X11/Xsession
 # ADD THIS LINE
 #/usr/bin/i3
 
 # Run stow to set up dotfiles
-# stow i3
-# stow nvim
-# stow bashrc somehow?
+$STOW_FOLDERS="i3,nvim,bashrc"
+pushd $DOTFILES
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+    stow -D $folder
+    stow $folder
+done
+popd
+"stow i3
+"stow nvim
+"stow bashrc
---
+++ readme.md
@@ -0,0 +1,15 @@
+# .dotfiles
+
+These are my config files for the various platforms I use
+ - Linux
+ - Windows
+ - MacOS
+
+ Each has it's own branch.
+
+To install, run the following commands:
+
+Linux: 
+ ```bash
+  curl -sL https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/linux/install.sh | sudo bash -
+ ```
