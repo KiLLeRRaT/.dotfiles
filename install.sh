ee66960acb19f336ba52603bad0fd256434ac974
Albert Gouws <albert@kilo.gouws.org>
2021-12-10 16:04:41 +1300

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# sudo apt update
+# sudo apt upgrade
+
+# Install i3
+# Install curl
+# Install xrdp
+# Install stow
+# sudo apt install xrdp i3 curl stow
+
+# Install Neovim
+# sudo add-apt-repository ppa:neovim-ppa/unstable
+# sudo apt update
+# sudo apt install neovim
+
+# CONFIG xrdp to start with i3
+# EDIT /etc/xrdp/startwm.sh
+# REMOVE THIS LINE
+#exec /bin/sh /etc/X11/Xsession
+# ADD THIS LINE
+#/usr/bin/i3
+
+# Run stow to set up dotfiles
+# stow i3
+# stow nvim
