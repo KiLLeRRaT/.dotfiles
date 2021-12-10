30f5d9d2e049a9515231242dd290118cd9968e44
Albert Gouws <albert@kilo.gouws.org>
2021-12-11 07:29:58 +1300

    Intall script

--- install.sh
+++ install.sh
@@ -23,13 +23,14 @@ echo Config xrdp to start with i3
 #exec /bin/sh /etc/X11/Xsession
 # ADD THIS LINE
 #/usr/bin/i3
-sudo sed -Ei.bak 's|test -x /etc/X11/Xsession \&\& exec /etc/X11/Xsession|# test -x /etc/X11/Xsession \&\& exec /etc/X11/Xsession|' /etc/xrdp/startwm.sh
-sudo sed -Ei.bak 's|exec /bin/sh /etc/X11/Xsession|# exec /bin/sh /etc/X11/Xsession|' /etc/xrdp/startwm.sh
 if grep -q "/usr/bin/i3" "/etc/xrdp/startwm.sh" ; then
 	# exists
 	echo "/usr/bin/i3 found, not adding it"
 else
 	# not exist
+
+	sudo sed -Ei.bak 's|test -x /etc/X11/Xsession \&\& exec /etc/X11/Xsession|# test -x /etc/X11/Xsession \&\& exec /etc/X11/Xsession|' /etc/xrdp/startwm.sh
+	sudo sed -Ei.bak 's|exec /bin/sh /etc/X11/Xsession|# exec /bin/sh /etc/X11/Xsession|' /etc/xrdp/startwm.sh
 	#sudo echo "/usr/bin/i3" >> /etc/xrdp/startwm.sh # DOES NOT WORK FOR SUDO.. USE BELOW
 	echo "/usr/bin/i3" | sudo tee -a /etc/xrdp/startwm.sh
 fi
