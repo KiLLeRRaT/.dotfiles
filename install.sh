7fa255069d28b3dd993af67da6d62aa4fb78406d
Albert Gouws <albert@gouws.org>
2021-12-12 13:43:32 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,16 @@ sudo apt -y upgrade
 echo Installing git, xrdp, i3, curl, stow
 sudo apt install -y git xrdp i3 curl stow neovim chrome-browser xclip
 
+echo Installing Chrome
+if [ $(dpkg-query -W -f='${Status}' chrome-browser 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+    sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+    rm -f google-chrome-stable_current_amd64.deb
+else
+	echo "Chrome found, not adding it"
+fi
+
 
 echo Clone dotfiles
 git clone https://github.com/KiLLeRRaT/.dotfiles.git
