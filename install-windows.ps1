4f2e36361e132046961672044ddef0c4d66a3d3f
Albert Gouws <albert@gouws.org>
2022-03-14 09:09:04 +1300

    Update windows install powershell script

--- install-windows.ps1
+++ install-windows.ps1
@@ -1,5 +1,6 @@
 #Requires -RunAsAdministrator
 
 # New-Item -ItemType HardLink -Path "~\.dotfiles\nvim\.config\nvim\init.vim" -Target "~\AppData\Local\nvim\init.vim"
-New-Item -ItemType HardLink -Force -Path "$home\AppData\Local\nvim\init.vim" -Target "$home\.dotfiles\nvim\.config\nvim\init.vim"
+# New-Item -ItemType HardLink -Force -Path "$home\AppData\Local\nvim\init.vim" -Target "$home\.dotfiles\nvim\.config\nvim\init.vim"
+New-Item -ItemType Junction -Force -Path "$home\AppData\Local\nvim" -Target "$home\.dotfiles\nvim\.config\nvim"
 
