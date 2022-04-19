3653f40a034c011be132048e14a0c647a984aea5
Albert Gouws <albert@gouws.org>
2022-04-19 14:29:39 +1200

    Added fonts I use and install script for Debian based systems

---
+++ fonts/install-debian.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# FROM: https://gist.github.com/matthewjberger/7dd7e079f282f8138a9dc3b045ebefa0#file-instructions-md
+# 1.) Download a [Nerd Font](http://nerdfonts.com/)
+# 2.) Unzip and copy to `~/.fonts`
+# 3.) Run the command `fc-cache -fv` to manually rebuild the font cache
+
+fc-cache -fv
