5f52c9bccfe98a1c4ae826f377bb3a5792a4fe09
Albert Gouws <albert@gouws.org>
2022-07-26 09:03:58 +1200

    Also remove the gp alias from PowerShell to make room for Git Push

--- PowerShell/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -48,3 +48,5 @@ Set-PSReadLineKeyHandler -Key Ctrl+r -Function ReverseSearchHistory
 
 # REMOVE gl, so that we can use gl.bat for git pull instead of Get-Location
 Remove-Alias -Force -Name gl
+Remove-Alias -Force -Name gp
+
