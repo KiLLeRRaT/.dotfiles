fa2990a26bffe0107529c17c56cf804ce13cc0c3
Albert Gouws <albert@gouws.org>
2022-07-31 08:30:25 +1200

    Moved plugin init to first in the scripts to make sure packer gets installed before anything else runs

--- nvim-lua/.config/nvim/lua/killerrat/plugins.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins.lua
@@ -1,3 +1,10 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.cmd [[packadd packer.nvim]]
+end
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
@@ -104,8 +111,10 @@ use { 'folke/tokyonight.nvim', branch = 'main' }
 
 
 
-
-
-
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
 
