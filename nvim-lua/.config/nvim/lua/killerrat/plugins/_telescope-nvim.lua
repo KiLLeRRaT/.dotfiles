ce65f595c87a59c384e943465b05395b584f1691
Albert Gouws <albert@gouws.org>
2022-08-01 09:27:45 +1200

    Further Telescope key bindings

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_telescope-nvim.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_telescope-nvim.lua
@@ -110,22 +110,33 @@ vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files find_command=rg,--i
 -- nnoremap <leader>fF :execute 'Telescope find_files default_text=' . "'" . expand('<cword>')<cr>
 vim.keymap.set("n", "<leader>fF", ":execute 'Telescope find_files default_text=' . '''' . expand('<cword>')<cr>")
 -- nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 -- nnoremap <leader>fG :execute 'Telescope live_grep default_text=' . expand('<cword>')<cr>
+vim.keymap.set("n", "<leader>fG", ":execute 'Telescope live_grep default_text=' . expand('<cword>')<cr>")
 
--- " nnoremap <leader>fR <cmd>lua require("telescope").extensions.live_grep_raw.live_grep_raw()<cr>
 -- nnoremap <leader>fR <cmd>lua require("telescope").extensions.live_grep_args.live_grep_args()<cr>
+vim.keymap.set("n", "<leader>fR", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>")
 
 -- nnoremap <leader>fb <cmd>Telescope buffers<cr>
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 -- nnoremap <leader>fk <cmd>Telescope help_tags<cr>
+vim.keymap.set("n", "<leader>fk", "<cmd>Telescope help_tags<cr>")
 -- nnoremap <leader>fm <cmd>Telescope keymaps<cr>
+vim.keymap.set("n", "<leader>fm", "<cmd>Telescope keymaps<cr>")
 -- nnoremap <leader>fc <cmd>Telescope git_commits<cr>
+vim.keymap.set("n", "<leader>fc", "<cmd>Telescope git_commits<cr>")
 -- nnoremap <leader>fr <cmd>Telescope git_branches<cr>
+vim.keymap.set("n", "<leader>fr", "<cmd>Telescope git_branches<cr>")
 -- " nnoremap <leader>fs <cmd>Telescope git_status<cr>
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>")
 -- nnoremap <leader>fs <cmd>Telescope colorscheme<cr>
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope colorscheme<cr>")
 -- nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
 
 -- " OVERRIDES THE STANDARD z= shortcut!
 -- nnoremap z= <cmd>Telescope spell_suggest<cr>
+vim.keymap.set("n", "z=", "<cmd>Telescope spell_suggest<cr>")
 
 -- " SEARCH MY OWN GBOX SCRIPTS
 -- lua require("killerrat")
