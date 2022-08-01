85f283dbaf238146b4aea9f2de389bba097510f7
Albert Gouws <albert@gouws.org>
2022-08-01 22:18:28 +1200

    Harpoon

--- nvim-lua/.config/nvim/lua/killerrat/plugins/init.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/init.lua
@@ -3,4 +3,5 @@ require("killerrat.plugins._colors")
 require("killerrat.plugins._whitespace-nvim")
 require("killerrat.plugins._telescope-nvim")
 require("killerrat.plugins._nerdtree")
+require("killerrat.plugins._harpoon")
 
