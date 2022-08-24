1982ee9df25d462796b0782e7bfa8d37505c13b5
Albert Gouws <albert@gouws.org>
2022-08-24 17:17:24 +1200

    nvim-lua: Don't source the generated packer file!

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_packer.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_packer.lua
@@ -157,7 +157,7 @@ packer.startup {
 	}
 }
 
-vim.cmd("source " .. compile_path)
+-- vim.cmd("source " .. compile_path)
 
 -- TO CHECK IF A PLUGIN IS INSTALLED
 function _G.plugin_loaded(plugin_name)
