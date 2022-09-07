dde3e18c9aebc1d44172eb3b9ec51a6dd5aff112
Albert Gouws <albert@gouws.org>
2022-09-08 10:25:12 +1200

    nvim-lua: installed gruvbox

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_colors.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_colors.lua
@@ -1,4 +1,3 @@
-vim.opt.background = "dark"
 vim.opt.termguicolors = true
 
 if _G.plugin_loaded("vim-colors-solarized") then
@@ -7,12 +6,14 @@ if _G.plugin_loaded("vim-colors-solarized") then
 end
 
 if _G.plugin_loaded("gruvbox") then
+	vim.opt.background = "dark"
 	vim.g.gruvbox_guisp_fallback = "bg"
 	vim.g.gruvbox_transparent_bg = 1
 	vim.cmd("colorscheme gruvbox")
 end
 
 if _G.plugin_loaded("tokyonight.nvim") then
+	vim.opt.background = "dark"
 	vim.g.tokyonight_transparent = true
 	vim.g.tokyonight_transparent_sidebar = true
 	vim.cmd("colorscheme tokyonight")
