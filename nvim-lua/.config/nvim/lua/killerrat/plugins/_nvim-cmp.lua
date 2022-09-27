5fd7016b741557be2367ff771510b6137b4428a7
Albert Gouws <albert@gouws.org>
2022-09-27 17:29:31 +1300

    nvim-lua: set cmp to look at all buffers for completion suggestions

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_nvim-cmp.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_nvim-cmp.lua
@@ -44,7 +44,27 @@ cmp.setup {
 	sources = {
 		{ name = 'nvim_lsp' },
 		{ name = 'path' },
-		{ name = "buffer" },
+		-- { name = "buffer" }, -- CURRENT BUFFER ONLY
+		{ -- ALL BUFFERS
+			name = 'buffer',
+			option = {
+				get_bufnrs = function()
+					return vim.api.nvim_list_bufs()
+				end
+			}
+		},
+		-- { -- VISIBLE BUFFERS
+		-- 			name = 'buffer',
+		-- 			option = {
+		-- 				get_bufnrs = function()
+		-- 					local bufs = {}
+		-- 					for _, win in ipairs(vim.api.nvim_list_wins()) do
+		-- 						bufs[vim.api.nvim_win_get_buf(win)] = true
+		-- 					end
+		-- 					return vim.tbl_keys(bufs)
+		-- 				end
+		-- 			}
+		-- 		}
 		{ name = 'nvim_lsp_signature_help' },
 		-- { name = 'vim-dadbod-completion' },
 		{ name = 'calc' },
