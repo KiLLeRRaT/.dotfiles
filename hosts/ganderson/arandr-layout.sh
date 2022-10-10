72cad1cfc47776c1600920d285aefe11700fac63
Albert Gouws <albert@gouws.org>
2022-10-11 09:31:22 +1300

    hosts-ganderson: added arandr-layout for triple displays

---
+++ hosts/ganderson/arandr-layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-0 --mode 2560x1440 --pos 0x467 --rotate normal --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 6400x467 --rotate normal
