3aa8eda46a45525a611f6842dccf94f532326c66
Albert Gouws <albert@gouws.org>
2022-10-12 10:05:56 +1300

    nvim-lua: switch: added days and months

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_switch-vim.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_switch-vim.lua
@@ -18,6 +18,10 @@ vim.cmd[[let g:switch_custom_definitions =
 		\   switch#NormalizedCaseWords(['starting', 'stopping']),
     \   switch#NormalizedCaseWords(['start', 'stop']),
     \   switch#NormalizedCase(['left', 'right']),
+    \   switch#NormalizedCaseWords(['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']),
+    \   switch#NormalizedCaseWords(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']),
+    \   switch#NormalizedCaseWords(['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']),
+    \   switch#NormalizedCaseWords(['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']),
     \ ]
 ]]
 
