c77200abc40cb6d252b73b90c121a7a1221eb533
Albert Gouws <albert@gouws.org>
2022-10-20 06:46:12 +1300

    i3status: set up host specific config, show br0 address for ganderson

--- i3-manjaro/.config/i3/generateHostConfig.sh
+++ i3-manjaro/.config/i3/generateHostConfig.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
+# RUN SED SCRIPT TO STRIP HOST SPECIFIC PARTS FROM i3status CONFIG FILE
+pushd ~/.config/i3status
+# echo "host is $HOST"
+rm config
+sed '/# <hostSpecificConfig>/,/# <\/hostSpecificConfig>/ {/# <config hostname="'$HOST'">/,/# <\/config>/!d}' config.allHosts > config
+popd
+
+
 # RUN SED SCRIPT TO STRIP HOST SPECIFIC PARTS FROM i3 CONFIG FILE
 pushd ~/.config/i3
 # echo "host is $HOST"
