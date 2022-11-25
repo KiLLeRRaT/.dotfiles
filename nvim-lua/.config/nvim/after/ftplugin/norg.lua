f3fa305834a52e973efa978e42072aaf03933141
Albert Gouws <albert@gouws.org>
2022-11-25 14:24:23 +1300

    nvim-lua: norg: surround current line in code tags normal mode

--- nvim-lua/.config/nvim/after/ftplugin/norg.lua
+++ nvim-lua/.config/nvim/after/ftplugin/norg.lua
@@ -4,5 +4,8 @@ vim.opt.shiftwidth = 2
 
 vim.keymap.set("n", "<localleader>nc", "<cmd>Neorg toggle-concealer<cr>")
 
+-- SURROUND CURRENT LINE IN CODE TAGS
+vim.keymap.set("n", "<localleader>sc", "O@code<esc>jo@end<esc>")
+
 -- SURROUND VISUAL SELECTION IN CODE TAGS
 vim.keymap.set("v", "<localleader>sc", "<esc>'<O@code<esc>'>o@end<esc>")
