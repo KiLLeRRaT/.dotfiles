2023ea1c3af23ebf440ada792c8dada07e41f9d0
Albert Gouws <albert@gouws.org>
2022-12-01 10:38:56 +1300

    powershell: Zoxide config

--- PowerShell/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -67,3 +67,8 @@ Remove-Alias -Force -Name gl
 Remove-Alias -Force -Name gp
 Remove-Alias -Force -Name gm
 
+# For zoxide v0.8.0+
+Invoke-Expression (& {
+		$hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+		(zoxide init --hook $hook powershell | Out-String)
+})
