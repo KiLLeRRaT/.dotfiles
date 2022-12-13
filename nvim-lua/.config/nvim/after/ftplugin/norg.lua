6844c32b8e3e274366e39ae53e596276dd799c0b
Albert Gouws <albert@gouws.org>
2022-12-14 12:24:11 +1300

    nvim-lua(ftnorg): expand folds on index.norg

--- nvim-lua/.config/nvim/after/ftplugin/norg.lua
+++ nvim-lua/.config/nvim/after/ftplugin/norg.lua
@@ -1,3 +1,4 @@
+-- vim.opt.foldlevel = 99
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
@@ -11,3 +12,17 @@ vim.keymap.set("n", "<localleader>sc", "O@code<esc>jo@end<esc>")
 vim.keymap.set("v", "<localleader>sc", "<esc>'<O@code<esc>'>o@end<esc>")
 
 vim.opt_local.spell = true
+
+
+-- FOLDLEVEL 99 WHEN OPENING INDEX.NORG
+local norgfiles_BufEnter_BufWinEnter= vim.api.nvim_create_augroup("norgfiles_BufEnter_BufWinEnter", { clear = true })
+vim.api.nvim_create_autocmd({"BufEnter, BufWinEnter"}, {
+	pattern = {
+		"**/index.norg"
+	},
+	callback = function()
+		vim.opt_local.foldlevel = 99
+		-- print("Set foldlevel to 99 for index.jorg")
+	end,
+	group = norgfiles_BufEnter_BufWinEnter
+})
