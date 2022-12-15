1ce5f68bf9ff86ec3861f8efa26bd16fc32dd83e
Albert Gouws <albert@gouws.org>
2022-12-15 18:01:03 +1300

    nvim-docker: tweak build script

--- nvim-docker/build.sh
+++ nvim-docker/build.sh
@@ -7,7 +7,15 @@ export BUILD_DATE=$(git rev-parse HEAD)
 # GET DATE IN yyyymmddHHmmss FORMAT
 # export BUILD_DATE=$(date +%Y%m%d%H%M%S)
 
-docker-compose build
+ # DO YOU WANT TO BUILD WITH NO CACHE?
+read -q "REPLY?Do you want build with --no-cache? [y/N] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+		docker-compose build --no-cache
+else
+		docker-compose build
+fi
+
 
  # DO YOU WANT TO PUSH THE IMAGE?
 read -q "REPLY?Do you want to push the image? [y/N] "
