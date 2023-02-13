40ebb28e1a7f631e7982fa738ea02e13a362b8fc
Albert Gouws <albert@gouws.org>
2023-02-13 21:24:39 +1300

    nvim-lua(grapple): Shortcut keys vor vertical splits

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_grapple-nvim.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_grapple-nvim.lua
@@ -11,7 +11,11 @@ vim.keymap.set("n", "<leader>h", function() require('grapple').select({ key = 1 
 vim.keymap.set("n", "<leader>j", function() require('grapple').select({ key = 2 }) end, { silent = true })
 vim.keymap.set("n", "<leader>k", function() require('grapple').select({ key = 3 }) end, { silent = true })
 vim.keymap.set("n", "<leader>l", function() require('grapple').select({ key = 4 }) end, { silent = true })
-vim.keymap.set("n", "<leader>;", function() require('grapple').select({ key = 5 }) end, { silent = true })
+
+vim.keymap.set("n", "<leader>H", function() vim.cmd[[vsp]]; require('grapple').select({ key = 1 }) end, { silent = true })
+vim.keymap.set("n", "<leader>J", function() vim.cmd[[vsp]]; require('grapple').select({ key = 2 }) end, { silent = true })
+vim.keymap.set("n", "<leader>K", function() vim.cmd[[vsp]]; require('grapple').select({ key = 3 }) end, { silent = true })
+vim.keymap.set("n", "<leader>L", function() vim.cmd[[vsp]]; require('grapple').select({ key = 4 }) end, { silent = true })
 
 -- vim.keymap.set("n", "<leader>H", ":vsp<cr>:lua require('harpoon.ui').nav_file(1)<cr>", { silent = true })
 -- vim.keymap.set("n", "<leader>J", ":vsp<cr>:lua require('harpoon.ui').nav_file(2)<cr>", { silent = true })
