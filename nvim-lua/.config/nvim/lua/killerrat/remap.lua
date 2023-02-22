48cd8cfbd7531802b202f0ba650f19f171796460
Albert Gouws <albert@gouws.org>
2023-02-22 23:15:24 +1300

    nvim-lua: <leader>; to close window again...

--- nvim-lua/.config/nvim/lua/killerrat/remap.lua
+++ nvim-lua/.config/nvim/lua/killerrat/remap.lua
@@ -234,5 +234,5 @@ for i = 1, 6 do
 end
 
 -- CLOSE CURRENT WINDOW
--- vim.keymap.set("n", "<leader>;", ":clo<cr>")
-vim.keymap.set("n", "<leader><cr>", ":clo<cr>")
+vim.keymap.set("n", "<leader>;", ":clo<cr>")
+-- vim.keymap.set("n", "<leader><cr>", ":clo<cr>")
