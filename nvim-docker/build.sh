979eaeb19852a4a0ea03a109e1914c13c3131903
Albert Gouws <albert@gouws.org>
2023-03-24 06:50:08 +1300

    nvim-docker: Build file updates

--- nvim-docker/build.sh
+++ nvim-docker/build.sh
@@ -24,6 +24,22 @@ then
 		docker-compose push
 fi
 
-# LIST IMAGES/REPOS
+# LIST IMAGES/REPOS:
 # https://dockerregistry.gouws.org/v2/_catalog
 # https://dockerregistry.gouws.org/v2/nvim-docker/tags/list
+#
+# DELETING IMAGES:
+#·https://dockerregistry.gouws.org/v2/nvim-docker/tags/list
+# DELETE	/v2/<name>/manifests/<reference>
+#
+#
+# FROM: https://stackoverflow.com/a/43786939/182888
+# curl -sSL -u "$(read 'u?Username: ';echo $u)" https://dockerregistry.gouws.org/v2/_catalog
+# curl -sSL -u "$(read 'u?Username: ';echo $u)" https://dockerregistry.gouws.org/v2/nvim-docker/tags/list
+# curl -sSL -u "$(read 'u?Username: ';echo $u)" -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -o /dev/null -w '%header{Docker-Content-Digest}' https://dockerregistry.gouws.org/v2/nvim-docker/manifests/<tag>
+# curl -sSL -u "$(read 'u?Username: ';echo $u)" -X DELETE https://dockerregistry.gouws.org/v2/nvim-docker/manifests/<digest>
+# /bin/registry garbage-collect /etc/docker/registry/config.yml
+
+
+
+
