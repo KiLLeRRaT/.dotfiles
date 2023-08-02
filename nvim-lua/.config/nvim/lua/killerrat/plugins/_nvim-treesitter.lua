cbdeba37bc507ee5a3c89f2e1ae59ebe21f3a638
Albert Gouws <albert@gouws.org>
2023-08-02 12:21:12 +1200

    nvim-luat(nvim-treesitter): Removed repeat mappings

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_nvim-treesitter.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_nvim-treesitter.lua
@@ -135,20 +135,25 @@ require'nvim-treesitter.configs'.setup {
 	 }
 	}
 
-	local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
 
+ -- THE BELOW IS NOT WORKING, IT BREAKS THE DOT COMMAND WHEN YOURE TRYING TO DOT REPEAT SOMETHING
+ -- DONE WITH T OR F
+	-- local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
 	-- Repeat movement with ; and ,
 	-- ensure ; goes forward and , goes backward regardless of the last direction
-	vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-	vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+	-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+	-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
 
 	-- vim way: ; goes to the direction you were moving.
 	-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
 	-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
 	-- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-	vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-	vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-	vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-	vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+	-- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+	-- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+	-- vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+	-- vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+ -- THE BELOW IS NOT WORKING, IT BREAKS THE DOT COMMAND WHEN YOURE TRYING TO DOT REPEAT SOMETHING
+ -- DONE WITH T OR F
 
