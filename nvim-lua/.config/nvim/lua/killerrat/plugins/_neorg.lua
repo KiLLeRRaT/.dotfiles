d043a206b467e4cbe17a618abd8dd094e3bab59f
Albert Gouws <albert@gouws.org>
2023-08-03 12:59:35 +1200

    nvim-lua(neorg): Sort out issue with namespace change

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_neorg.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_neorg.lua
@@ -49,7 +49,7 @@ require('neorg').setup {
 
 
 -- FROM : https://github.com/nvim-neorg/neorg-telescope
-local neorg_callbacks = require("neorg.callbacks")
+local neorg_callbacks = require("neorg.core.callbacks")
 neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
 		-- Map all the below keybinds only when the "norg" mode is active
 		keybinds.map_event_to_mode("norg", {
