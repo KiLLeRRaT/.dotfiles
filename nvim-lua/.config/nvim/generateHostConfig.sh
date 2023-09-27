2c349cb5227580096a095a44c2dab9329aa1162a
Albert Gouws <albert@gouws.org>
2023-09-28 09:50:52 +1300

    nvim-lua(generateHostConfig): Logging tweaks

--- nvim-lua/.config/nvim/generateHostConfig.sh
+++ nvim-lua/.config/nvim/generateHostConfig.sh
@@ -1,17 +1,16 @@
 #!/bin/zsh
 
+echo "host is $HOST"
+
 # RUN SED SCRIPT TO STRIP HOST SPECIFIC PARTS FROM i3status CONFIG FILE
 pushd ~/.dotfiles/i3-manjaro/.config/i3status
-# echo "host is $HOST"
 rm config
 sed '/# <hostSpecificConfig>/,/# <\/hostSpecificConfig>/ {/# <config hostname="'$HOST'">/,/# <\/config>/!d;}' config.allHosts > config
 popd
 
 
 # RUN SED SCRIPT TO STRIP HOST SPECIFIC PARTS FROM i3 CONFIG FILE
-# pushd ~/.config/i3
 pushd ~/.dotfiles/i3-manjaro/.config/i3
-# echo "host is $HOST"
 rm config
 sed '/# <hostSpecificConfig>/,/# <\/hostSpecificConfig>/ {/# <config hostname="'$HOST'">/,/# <\/config>/!d;}' config.allHosts > config
 popd
@@ -19,7 +18,6 @@ popd
 
 # RUN SED SCRIPT TO STRIP HOST SPECIFIC PARTS FROM alacritty CONFIG FILE
 pushd ~/.dotfiles/alacritty/.config/alacritty/
-# echo "host is $HOST"
 rm alacritty.yml
 sed '/# <hostSpecificConfig>/,/# <\/hostSpecificConfig>/ {/# <config hostname="'$HOST'">/,/# <\/config>/!d;}' alacritty.allHosts.yml > alacritty.yml
 popd
