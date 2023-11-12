4eda3f22c7efca000062526082a2277423b8d081
Albert Gouws <albert@gouws.org>
2023-11-13 09:37:15 +1300

    install-arch-agouws: Tweaks and cleanup

--- install-arch-agouws.sh
+++ install-arch-agouws.sh
@@ -20,6 +20,7 @@
 
 # Exit when any command fails
 set -e
+username=albert
 
 echo -e "\033[32m ----------------------------------------\033[0m"
 echo -e "\033[32m Installing Arch Linux\033[0m"
@@ -86,7 +87,8 @@ pacman --noconfirm -Syu \
 	virt-manager libvirt qemu virt-viewer swtpm \
 	zathura zathura-pdf-poppler \
 	sshfs \
-	signal-desktop discord
+	signal-desktop discord \
+	wireplumber pipewire-pulse pulseaudio pavucontrol playerctl
 
 echo "Starting sshd"
 sudo systemctl start sshd
@@ -100,25 +102,20 @@ echo "Starting Bluetooth"
 sudo systemctl start bluetooth
 sudo systemctl enable bluetooth
 
-
-echo "Reboot now, and continue the script from the new system as root"
-exit 1
-
-
 echo "Starting NetworkManager"
 sudo systemctl enable NetworkManager.service
 sudo systemctl start NetworkManager.service
 
-username=albert
+echo "From here, run as the user in the new system"
+exit 1
 
 echo "Cloning dotfiles"
 pushd /home/$username
 git clone https://github.com/killerrat/.dotfiles
 popd
 
-
-echo "From here, run as the user in the new system"
-exit 1
+echo "Generate host specific configs"
+~/.dotfiles/nvim-lua/.config/nvim/generateHostConfig.sh
 
 echo "Running stow"
 pushd ~/.dotfiles/hosts/arch-agouws
@@ -129,33 +126,13 @@ pushd ~/.dotfiles
 stow alacritty dmenurc dosbox dunst fonts gitconfig gtk-2.0 gtk-3.0 gtk-4.0 i3-manjaro nvim-lua oh-my-posh picom ranger tmux zshrc
 popd
 
-
-echo "Generate host specific configs"
-~/.dotfiles/nvim-lua/.config/nvim/generateHostConfig.sh
-
-
 echo "Let's copy our gtk configs to /root, so that root has the same theme"
 sudo cp /home/albert/.dotfiles/gtk-2.0/.gtkrc-2.0 /root
 sudo mkdir -p /root/.config
 sudo cp -r /home/albert/.dotfiles/gtk-3.0/.config/gtk-3.0 /root/.config
 sudo cp -r /home/albert/.dotfiles/gtk-4.0/.config/gtk-4.0 /root/.config
 
-
-sudo systemctl enable NetworkManager.service
-sudo systemctl start NetworkManager.service
-echo "If running on the MacBook, you need to set up brcmfmac43602-pcie. Do you want to set up brcmfmac43602-pcie? (y/n)"
-read setup_brcmfmac43602_pcie
-if [ "$setup_brcmfmac43602_pcie" == "y" ]; then
-	sudo cp /home/$USER/.dotfiles/hosts/arch-agouwsmacbookpro/brcmfmac43602-pcie.txt /lib/firmware/brcm/.
-	sudo cp /home/$USER/.dotfiles/hosts/arch-agouwsmacbookpro/brcmfmac43602-pcie.txt /lib/firmware/brcm/brcmfmac43602-pcie.Apple\ Inc.-MacBookPro13,3.txt
-fi
-sudo systemctl restart NetworkManager.service
-
 chsh -s /bin/zsh albert
-
-echo "From here, run as the user in X"
-exit 1
-
 
 echo -e "\033[32m ----------------------------------------\033[0m"
 echo -e "\033[32m Installing AUR Packages\033[0m"
@@ -175,18 +152,6 @@ installAurPackage() {
 installAurPackage oh-my-posh
 installAurPackage brave-bin
 
-echo "If running on the MacBook, you probably want to install macbook12-spi-driver-dkms. Also swap fn and ctrl, and permanently make the F keys display on the touchbar. Do you want to do this? (y/n)"
-read update_mkinitcpio_fnremap
-if [ "$update_mkinitcpio_fnremap" == "y" ]; then
-	installAurPackage macbook12-spi-driver-dkms
-	sudo sed -i.bak3 '/^MODULES=(/c\MODULES=(apple_ib_tb applespi intel_lpss_pci spi_pxa2xx_platform)' /etc/mkinitcpio.conf
-	echo 'options apple_ib_tb fnmode=2' | sudo tee /etc/modprobe.d/apple_ib_tb.conf
-	echo 'options apple_ib_tb idle_timeout=60' | sudo tee --append /etc/modprobe.d/apple_ib_tb.conf
-	echo 'options applespi fnremap=1' | sudo tee /etc/modprobe.d/applespi.conf
-	sudo mkinitcpio -P
-fi
-
-
 echo "Set up seahorse and create a default keyring. This is needed for 1Password otherwise it keeps asking the 2FA prompt again and again."
 # FROM: https://1password.community/discussion/127523/1password-and-gnome-keyring-for-2fa-saving-on-archlinux
 seahorse
@@ -202,10 +167,10 @@ sudo bash -c "cat > /etc/pam.d/login" << 'EOF'
 auth       required     pam_securetty.so
 auth       requisite    pam_nologin.so
 auth       include      system-local-login
-auth       optional     pam_gnome_keyring.so # ADD THIS LINE AT END OF AUTH
+auth       optional     pam_gnome_keyring.so
 account    include      system-local-login
 session    include      system-local-login
-session    optional     pam_gnome_keyring.so auto_start # ADD THIS LINE AT END OF SESSION
+session    optional     pam_gnome_keyring.so auto_start
 password   include      system-local-login
 EOF
 
@@ -214,13 +179,12 @@ sudo nvim -d /etc/pam.d/login /tmp/login.tmp
 
 installAurPackage i3exit
 
-sudo pacman --noconfirm -S python-antlr4 python-semantic-version python-systemd python-tomlkit python-typeguard python-watchdog refind
-installAurPackage python-injector
-installAurPackage python-pid
-installAurPackage python-transitions
-installAurPackage refind-btrfs
-installAurPackage refind-theme-dracula
-installAurPackage gmux_backlight
+# I THINK THIS WAS USED BY REFIND WHICH WE ONLY INSTALL ON THE MACBOOK
+# sudo pacman --noconfirm -S python-antlr4 python-semantic-version python-systemd python-tomlkit python-typeguard python-watchdog
+# installAurPackage python-injector
+# installAurPackage python-pid
+# installAurPackage python-transitions
+
 installAurPackage otf-san-francisco
 installAurPackage otf-san-francisco-mono
 installAurPackage pa-applet-git
@@ -244,35 +208,13 @@ systemctl start systemd-resolved.service
 installAurPackage icaclient #Citrix workspace app/Citrix receiver
 # COPYING AND PASTING NOT WORKING IN icaclient, DISABLE KLIPPER OR CLIPBOARD MANAGER!
 
-
-
 installAurPackage nvm
 source /usr/share/nvm/init-nvm.sh
 nvm install --lts
 nvm use --lts
 
-
-echo "If running on the MacBook, you need to install snd_hda_macbookpro. Do you want to do this? (y/n)"
-read snd_hda_macbookpro
-if [ "$snd_hda_macbookpro" == "y" ]; then
-	pushd ~/source-aur
-	git clone https://github.com/davidjo/snd_hda_macbookpro.git
-	cd snd_hda_macbookpro/
-	#run the following command as root or with sudo
-	sudo ./install.cirrus.driver.sh
-	echo "You need to reboot after doing the Sound Install. Do you want to reboot? (y/n)"
-	read reboot
-	if [ "$reboot" == "y" ]; then
-		sudo reboot now
-	fi
-fi
-sudo pacman -S wireplumber pipewire-pulse pulseaudio pavucontrol playerctl
-popd
-
-
 echo Configure dmenu
 sudo ln -s ~/.dotfiles/scripts/dmenu_recency /usr/local/bin/dmenu_recency
-
 
 echo "Enable and start Docker? (y/n)"
 read enable_docker
@@ -280,28 +222,4 @@ if [ "$enable_docker" == "y" ]; then
 	sudo systemctl enable docker
 	sudo systemctl start docker
 fi
-
-echo "Sort out the terminal TTY screen buffer size"
-sudo cp ~/.dotfiles/hosts/arch-agouwsmacbookpro/etc/systemd/system/set-display-resolution.service /etc/systemd/system/set-display-resolution.service
-sudo systemctl enable set-display-resolution.service
-sudo systemctl start set-display-resolution.service
-
-# sudo cp ~/.dotfiles/hosts/arch-agouwsmacbookpro/etc/systemd/system/disable-dgpu.service /etc/systemd/system/disable-dgpu.service
-# sudo systemctl enable disable-dgpu.service
-# sudo systemctl start disable-dgpu.service
-
-echo Configure graphics to use Intel only
-echo "blacklist amdgpu" | sudo tee /etc/modprobe.d/blacklist-amdgpu.conf
-pushd ~/source-aur
-git clone https://github.com/0xbb/gpu-switch
-cd gpu-switch
-sudo ./gpu-switch -i
-echo "You need to reboot after doing the Graphics Config to blacklist AMD GPU. Do you want to reboot? (y/n)"
-read reboot
-if [ "$reboot" == "y" ]; then
-	sudo reboot now
-fi
-popd
-
-
 
