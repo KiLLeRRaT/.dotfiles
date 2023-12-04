8456b332ad293400528db6d535f860c3df9486ff
Albert Gouws <albert@gouws.org>
2023-12-04 17:04:39 +1300

    i3-manjaro: Order displays and set display variables

---
+++ hosts/arch-agouws/arandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --mode 2560x1440 --pos 6400x720 --rotate normal --output DP-0 --mode 2560x1440 --pos 0x720 --rotate normal --output DP-1 --off --output HDMI-1 --primary --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-2 --off --output DP-3 --off
