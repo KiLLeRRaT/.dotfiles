eae240313cfd212f386e8db0cb0af45e3270fd3b
Albert Gouws <albert@gouws.org>
2024-03-08 05:25:16 +1300

    nvim-lua(lsp): gd for cs files uses omnisharp_extended, everything else uses telescope lsp_definitions

--- nvim-lua/.config/nvim/after/ftplugin/cs.lua
+++ nvim-lua/.config/nvim/after/ftplugin/cs.lua
@@ -5,3 +5,5 @@ vim.opt_local.foldlevel = 99
 -- FROM: https://github.com/nickspoons/vim-cs/issues/68#issuecomment-1718489232
 -- ALSO SEE: https://neovim.io/doc/user/indent.html
 vim.cmd[[setlocal cinoptions=(1s,J1]]
+
+vim.keymap.set("n", "gd", ":lua require('omnisharp_extended').telescope_lsp_definitions()<cr>")
