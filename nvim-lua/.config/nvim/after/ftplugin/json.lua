688f75e0b235ae184ec71c00b5ae1b52c8989eea
Albert Gouws <albert@gouws.org>
2024-05-31 11:05:42 +1200

    nvim-lua(ftplugin/json): Set filetype to jsonc when loading appsettings*.json files, also set comment string

--- nvim-lua/.config/nvim/after/ftplugin/json.lua
+++ nvim-lua/.config/nvim/after/ftplugin/json.lua
@@ -1,3 +1,16 @@
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt_local.foldlevel = 99
+
+-- IF THE FILENAME IS APPSETTINGS*.JSON, THEN SET FILETYPE TO JSONC
+local appsettingsJsonFiles_BufEnter_BufWinEnter= vim.api.nvim_create_augroup("appsettingsJsonFiles_BufEnter_BufWinEnter", { clear = true })
+vim.api.nvim_create_autocmd({"BufReadPre", "BufEnter", "BufWinEnter"}, {
+	pattern = {
+		"**/appsettings*.json"
+	},
+	callback = function()
+		vim.bo.filetype = "jsonc"
+		vim.opt.commentstring = "// %s"
+	end,
+	group = appsettingsJsonFiles_BufEnter_BufWinEnter
+})
