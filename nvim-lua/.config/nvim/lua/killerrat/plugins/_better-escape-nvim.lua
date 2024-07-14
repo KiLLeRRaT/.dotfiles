f230108e1350041f508531143043a2e58d654448
Albert Gouws <albert@gouws.org>
2024-07-14 19:54:06 +1200

    nvim-lua(_better-escape-nvim): New default config after rewrite

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_better-escape-nvim.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_better-escape-nvim.lua
@@ -1,10 +1,35 @@
 require("better_escape").setup {
-		mapping = {"jk"}, -- a table with mappings to use
-		timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-		clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-		keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
-		-- example(recommended)
-		-- keys = function()
-		--   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-		-- end,
+		timeout = vim.o.timeoutlen,
+		default_mappings = true,
+		mappings = {
+				i = {
+						j = {
+								-- These can all also be functions
+								k = "<Esc>",
+								j = "<Esc>",
+						},
+				},
+				c = {
+						j = {
+								k = "<Esc>",
+								j = "<Esc>",
+						},
+				},
+				t = {
+						j = {
+								k = "<Esc>",
+								j = "<Esc>",
+						},
+				},
+				v = {
+						j = {
+								k = "<Esc>",
+						},
+				},
+				s = {
+						j = {
+								k = "<Esc>",
+						},
+				},
+		},
 }
