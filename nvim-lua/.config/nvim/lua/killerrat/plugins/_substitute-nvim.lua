1634eff74bc790a239eeb36aad79bb56cf7456a0
Albert Gouws <albert@gouws.org>
2024-08-08 09:19:16 +1200

    nvim-lua(_substitute-nvim): Added substitute in range, and exchange keybinds

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_substitute-nvim.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_substitute-nvim.lua
@@ -15,13 +15,24 @@ require("substitute").setup {
 	},
 }
 
-vim.keymap.set("n", "s", "<cmd>lua require('substitute').operator()<cr>", { noremap = true })
-vim.keymap.set("n", "ss", "<cmd>lua require('substitute').line()<cr>", { noremap = true })
-vim.keymap.set("n", "S", "<cmd>lua require('substitute').eol()<cr>", { noremap = true })
-vim.keymap.set("x", "s", "<cmd>lua require('substitute').visual()<cr>", { noremap = true })
+vim.keymap.set("n", "s", require('substitute').operator, { noremap = true })
+vim.keymap.set("n", "ss",require('substitute').line, { noremap = true })
+vim.keymap.set("n", "S", require('substitute').eol, { noremap = true })
+vim.keymap.set("x", "s", require('substitute').visual, { noremap = true })
 
 
 -- vim.keymap.set("n", "<leader>s", "<cmd>lua require('substitute.range').operator()<cr>", { noremap = true })
 -- vim.keymap.set("x", "<leader>s", "<cmd>lua require('substitute.range').visual()<cr>", { noremap = true })
 -- vim.keymap.set("n", "<leader>ss", "<cmd>lua require('substitute.range').word()<cr>", { noremap = true })
 
+-- FROM: https://github.com/gbprod/substitute.nvim#-substitute-over-range-motion
+-- SUBSTITUTE IN RANGE
+vim.keymap.set("n", "<leader>s", require('substitute.range').operator, { noremap = true })
+vim.keymap.set("x", "<leader>s", require('substitute.range').visual, { noremap = true })
+vim.keymap.set("n", "<leader>ss", require('substitute.range').word, { noremap = true })
+
+-- EXCHANGE
+vim.keymap.set("n", "sx", require('substitute.exchange').operator, { noremap = true })
+vim.keymap.set("n", "sxx", require('substitute.exchange').line, { noremap = true })
+vim.keymap.set("x", "X", require('substitute.exchange').visual, { noremap = true })
+vim.keymap.set("n", "sxc", require('substitute.exchange').cancel, { noremap = true })
