5795063d18cc75b7e4b254b0ca6c89eef1587d72
Albert Gouws <albert@gouws.org>
2024-08-30 13:35:11 +1200

    nvim-lua(_grapple-nvim): Comments on how to not update the cursor location

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_grapple-nvim.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_grapple-nvim.lua
@@ -25,3 +25,17 @@ vim.keymap.set("n", "<leader>L", function() vim.cmd[[vsp]]; p.select({ index = 4
 -- -- vim.keymap.set("n", "<leader>K", ":vsp<cr>:lua require('harpoon.ui').nav_file(3)<cr>", { silent = true })
 -- -- vim.keymap.set("n", "<leader>L", ":vsp<cr>:lua require('harpoon.ui').nav_file(4)<cr>", { silent = true })
 
+-- FROM: https://github.com/cbochs/grapple.nvim/issues/72#issuecomment-1822838338
+-- clear the autocmd so that we keep the old line number instead of updating it as we move around
+-- the file and leave the buffer
+-- The problem with removing these is that we never get the cursor position set, it *ONLY* gets set
+-- when these are run somehow
+-- vim.api.nvim_clear_autocmds({group = "Grapple", event = 'BufLeave'})
+-- vim.api.nvim_clear_autocmds({group = "Grapple", event = 'BufWinLeave'})
+-- vim.api.nvim_clear_autocmds({group = "Grapple", event = 'QuitPre'})
+
+-- Grapple  BufWinLeave
+-- auto commands are created in /home/albert/.local/share/nvim/lazy/grapple.nvim/plugin/grapple.lua
+-- want to disable these (the grapple.touch so that it doesn't update the cursor position...
+
+-- https://github.com/cbochs/grapple.nvim/issues/118
