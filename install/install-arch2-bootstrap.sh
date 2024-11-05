843e5b5b00d2bd9b58749c270dc605cc4e83293d
Albert Gouws <albert@gouws.org>
2024-11-06 11:48:52 +1300

    install(install-arch2): Added bootstrap script

---
+++ install/install-arch2-bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+curl -LOJ https://u.gouws.org/install/install-arch2.10-partition.sh
+curl -LOJ https://u.gouws.org/install/install-arch2.20-mount.sh
+curl -LOJ https://u.gouws.org/install/install-arch2.30-install.sh
+curl -LOJ https://u.gouws.org/install/install-arch2.50-arch-chroot.sh
+curl -LOJ https://u.gouws.org/install/install-arch2.90-umount.sh
+
