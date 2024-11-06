3333da4f65c3741230ea662655adfeb606286082
Albert Gouws <albert@gouws.org>
2024-11-06 14:16:06 +1300

    install(install-arch2-bootstrap): No cache

--- install/install-arch2-bootstrap.sh
+++ install/install-arch2-bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-curl -LOJ https://u.gouws.org/install/install-arch2.10-partition.sh
-curl -LOJ https://u.gouws.org/install/install-arch2.20-mount.sh
-curl -LOJ https://u.gouws.org/install/install-arch2.30-install.sh
-curl -LOJ https://u.gouws.org/install/install-arch2.50-arch-chroot.sh
-curl -LOJ https://u.gouws.org/install/install-arch2.90-umount.sh
-curl -LOJ https://u.gouws.org/install/install-arch2.variables.sh
+curl -H 'Cache-Control: no-cache, no-store' -LOJ https://u.gouws.org/install/install-arch2.10-partition.sh
+curl -H 'Cache-Control: no-cache, no-store' -LOJ https://u.gouws.org/install/install-arch2.20-mount.sh
+curl -H 'Cache-Control: no-cache, no-store' -LOJ https://u.gouws.org/install/install-arch2.30-install.sh
+curl -H 'Cache-Control: no-cache, no-store' -LOJ https://u.gouws.org/install/install-arch2.50-arch-chroot.sh
+curl -H 'Cache-Control: no-cache, no-store' -LOJ https://u.gouws.org/install/install-arch2.90-umount.sh
+curl -H 'Cache-Control: no-cache, no-store' -LOJ https://u.gouws.org/install/install-arch2.variables.sh
 chmod u+x *.sh
 echo "Done"
 
