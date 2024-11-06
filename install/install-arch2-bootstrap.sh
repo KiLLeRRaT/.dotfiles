6af1d112d84795a95b2e3818aa78bd42719b5b73
Albert Gouws <albert@gouws.org>
2024-11-06 14:00:38 +1300

    install(install-arch2-bootstrap): Added variables file

--- install/install-arch2-bootstrap.sh
+++ install/install-arch2-bootstrap.sh
@@ -6,6 +6,7 @@ curl -LOJ https://u.gouws.org/install/install-arch2.20-mount.sh
 curl -LOJ https://u.gouws.org/install/install-arch2.30-install.sh
 curl -LOJ https://u.gouws.org/install/install-arch2.50-arch-chroot.sh
 curl -LOJ https://u.gouws.org/install/install-arch2.90-umount.sh
+curl -LOJ https://u.gouws.org/install/install-arch2.variables.sh
 chmod u+x *.sh
 echo "Done"
 
