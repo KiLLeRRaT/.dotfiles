ab0548f80e778e7e6af9be7bb358df710476f781
Albert Gouws <albert@gouws.org>
2025-02-20 14:46:11 +1300

    nvim-lua(_vim-dadbod-ui): fix: fix the issue with the plugin not loading

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_vim-dadbod-ui.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_vim-dadbod-ui.lua
@@ -1,7 +1,13 @@
-local p = "_vim-dadbod-ui"; if (not require('killerrat.plugins._lazy-nvim').LazyHasPlugin(p)) then return end
+local p = "vim-dadbod-ui"; if (not require('killerrat.plugins._lazy-nvim').LazyHasPlugin(p)) then return end
 -- jdbc:sqlserver://[serverName[\instanceName][:portNumber]][;property=value[;property=value]]
 -- jdbc:sqlserver://sqlserver.gouws.org;user=sa;password=your_password;database=your_db
 -- sqlserver://sqlserver.gouws.org;user=sa;password=your_password;database=your_db
 -- aur go-sqlcmd package is also needed
 
 -- server=sql.orca.kr.devlocal\sql2016;uid=OrcaWebUser;pwd=fr3ufouz;database=Orca
+
+-- SET VIM variable using neovim lua interface
+-- g:db_ui_execute_on_save = 0
+
+vim.g.db_ui_execute_on_save = 0
+
