3b4817edbe738d079a8a8ac2d553f76361abefc0
Albert Gouws <albert@gouws.org>
2025-02-20 14:45:41 +1300

    nvim-lua(ftplugin): comment out special formatting line for cs

--- nvim-lua/.config/nvim/after/ftplugin/cs.lua
+++ nvim-lua/.config/nvim/after/ftplugin/cs.lua
@@ -4,6 +4,8 @@ vim.opt_local.foldlevel = 99
 
 -- FROM: https://github.com/nickspoons/vim-cs/issues/68#issuecomment-1718489232
 -- ALSO SEE: https://neovim.io/doc/user/indent.html
-vim.cmd[[setlocal cinoptions=(1s,J1]]
+-- -- Wed 19 Feb 2025 11:12 Taken it out just to test since changing to roslyn lsp instead of
+-- omnisharp, not sure if it will make a diff, then we can just put it back in.
+-- vim.cmd[[setlocal cinoptions=(1s,J1]]
 
 -- vim.keymap.set("n", "gd", ":lua require('omnisharp_extended').telescope_lsp_definitions()<cr>")
