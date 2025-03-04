1ac87a01073e59d1d7f4cc8c6eb70b3dd578113f
Albert Gouws <albert@gouws.org>
2025-03-05 12:07:12 +1300

    install(neovim): Tweaks the directory names

--- install/install-nvim-from-tar.sh
+++ install/install-nvim-from-tar.sh
@@ -10,8 +10,8 @@ curl -L -o nvim.tar.gz https://github.com/neovim/neovim/releases/latest/download
 tar -xzf nvim.tar.gz
 
 # Move the extracted stuff to /usr/local/bin, and /usr/local/share
-rsync -auv nvim/ /usr/local/
+rsync -auv nvim-linux-x86_64/ /usr/local/
 
-rm -rf nvim nvim.tar.gz
+rm -rf nvim-linux-x86_64 nvim.tar.gz
 
 popd
