daaf09a3550a4c39babc0ede2c2568a2f80fa259
Albert Gouws <albert@gouws.org>
2025-03-05 12:12:57 +1300

    install(neovim): run rsync with sudo

--- install/install-nvim-from-tar.sh
+++ install/install-nvim-from-tar.sh
@@ -10,7 +10,7 @@ curl -L -o nvim.tar.gz https://github.com/neovim/neovim/releases/latest/download
 tar -xzf nvim.tar.gz
 
 # Move the extracted stuff to /usr/local/bin, and /usr/local/share
-rsync -auv nvim-linux-x86_64/ /usr/local/
+sudo rsync -auv nvim-linux-x86_64/ /usr/local/
 
 rm -rf nvim-linux-x86_64 nvim.tar.gz
 
