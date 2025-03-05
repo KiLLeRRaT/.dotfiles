9c44083f131c7b11083e62271ab299ecd9d98885
Albert Gouws <albert@gouws.org>
2025-03-05 14:41:03 +1300

    install(install-windows): Update commands and use winget instead

--- install/install-windows.ps1
+++ install/install-windows.ps1
@@ -7,66 +7,33 @@
 
 Write-Host Configuring NVM
 Write-Host ----------------------------------------
-$installPath = "C:\Users\Albert\AppData\Roaming\nvm"
-if (-not (Test-Path -Path $installPath))
-{
-	$nvmUrl =  "https://github.com/coreybutler/nvm-windows/releases/latest/download/nvm-setup.zip"
-	$extractPath = "C:\Temp\nvm\"
-	$downloadZipFile = $extractPath + $(Split-Path -Path $nvmUrl -Leaf)
-	mkdir $extractPath
-	Invoke-WebRequest -Uri $nvmUrl -OutFile $downloadZipFile
-	$extractShell = New-Object -ComObject Shell.Application
-	$extractFiles = $extractShell.Namespace($downloadZipFile).Items()
-	$extractShell.NameSpace($extractPath).CopyHere($extractFiles)
-	pushd $extractPath
-	Start-Process .\nvm-setup.exe -Wait
-	popd
-	Read-Host -Prompt "Setup done, now close the command window, and run this script again in a new elevated window. Press any key to continue"
-	Exit
-}
-else
-{
-	Write-Host Detected that NVM is already installed, so now using it to install NodeJS LTS
-	pushd $installPath
-	.\nvm.exe install lts
-	.\nvm.exe use lts
-	popd
-}
+winget install --id=CoreyButler.NVMforWindows  -e
+nvm install lts
+nvm use lts
 
 
 Write-Host Configuring WSL
-wsl --install -d Ubuntu-20.04
-
-
-Write-Host Install Chocolatey
-Write-Host ----------------------------------------
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+wsl --install
+wsl --install -d Ubuntu-24.04
 
 
 Write-Host Install Applications
 Write-Host ----------------------------------------
-choco install ripgrep # https://github.com/BurntSushi/ripgrep
-choco install fd # https://github.com/sharkdp/fd
-choco install sudo # ALLOWS USING sudo IN POWERSHELL!
-choco install win32yank # Neovim clipboard support in WSL, FROM: https://stackoverflow.com/a/67229362/182888
+winget install BurntSushi.ripgrep.MSVCripgrep # https://github.com/BurntSushi/ripgrep
+winget install sharkdp.fd # https://github.com/sharkdp/fd
+# choco install sudo # ALLOWS USING sudo IN POWERSHELL!
+# choco install win32yank # Neovim clipboard support in WSL, FROM: https://stackoverflow.com/a/67229362/182888
 
 Write-Host Configuring Neovim
 Write-Host ----------------------------------------
 New-Item -ItemType Junction -Force `
 	-Path "$home\AppData\Local\nvim" `
-	-Target "$home\.dotfiles\nvim\.config\nvim"
+	-Target "$home\.dotfiles\nvim-lua\.config\nvim"
 
 
 Write-Host Configuring Oh My Posh
 Write-Host ----------------------------------------
-# Install-Module oh-my-posh -Scope CurrentUser
-# Install-Module posh-git -Scope CurrentUser
-# [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:POSH_PATH", "Machine")
-
-# winget install oh-my-posh
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+winget install JanDeDobbeleer.OhMyPosh -s winget
 New-Item -ItemType Junction -Force -Path "$home\.omp" -Target "$home\.dotfiles\oh-my-posh\.omp"
 
 
@@ -89,24 +56,5 @@ Move-Item -Force "$home\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3
 New-Item -ItemType HardLink -Force `
 	-Path "$home\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" `
 	-Target "$home\.dotfiles\windows-terminal\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-
-
-Write-Host Registry Tweaks
-Write-Host ----------------------------------------
-
-## Show hidden files
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name Hidden 1
-
-## Show file extensions for known file types
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name HideFileExt 0
-
-## Never Combine taskbar buttons when the taskbar is full
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name TaskbarGlomLevel -Value 2
-
-# Taskbar small icons
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name TaskbarSmallIcons -Value 1
-
-# Set Windows to use UTC time instead of local time for system clock
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name RealTimeIsUniversal -Value 1
 
 
