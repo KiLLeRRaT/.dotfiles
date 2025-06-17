70a74945eada966adc43ee4d476251ff11340fb7
Albert Gouws <albert@gouws.org>
2025-06-18 08:56:48 +1200

    install(install-fzf-from-tar): Update to install to /usr/local/bin

--- install/install-fzf-from-tar.sh
+++ install/install-fzf-from-tar.sh
@@ -3,9 +3,9 @@ set -e
 
 pushd ~/Downloads
 
-curl -L -o fzf.tar.gz https://github.com/junegunn/fzf/releases/latest/download/fzf-0.61.1-linux_amd64.tar.gz
+curl -L -o fzf.tar.gz https://github.com/junegunn/fzf/releases/latest/download/fzf-0.62.0-linux_amd64.tar.gz
 tar -xzf fzf.tar.gz
-sudo cp fzf /usr/bin
+sudo cp fzf /usr/local/bin
 rm -rf fzf fzf.tar.gz
 
 popd
