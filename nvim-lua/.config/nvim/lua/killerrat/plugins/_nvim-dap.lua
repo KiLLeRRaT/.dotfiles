020c6dc1c413f0ba1836695d5bdcc3858a09df74
Albert Gouws <albert@gouws.org>
2025-06-26 11:28:10 +1200

    nvim-lua(nvim-dap): Attach and Launch working well

--- nvim-lua/.config/nvim/lua/killerrat/plugins/_nvim-dap.lua
+++ nvim-lua/.config/nvim/lua/killerrat/plugins/_nvim-dap.lua
@@ -8,41 +8,69 @@ dap.adapters.coreclr = {
 	args = {'--interpreter=vscode'}
 }
 
--- dap.configurations.cs = {
--- 	{
--- 		type = "coreclr",
--- 		name = "launch - netcoredbg",
--- 		request = "launch",
--- 		console = "integratedTerminal",
--- 		program = function()
--- 			return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
--- 			-- return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '', 'file')
--- 		end,
--- 	},
--- }
+-- FROM: https://github.com/mfussenegger/nvim-dap/wiki/Cookbook#making-debugging-net-easier
+-- vim.g.dotnet_build_project = function()
+--     local default_path = vim.fn.getcwd() .. '/'
+--     if vim.g['dotnet_last_proj_path'] ~= nil then
+--         default_path = vim.g['dotnet_last_proj_path']
+--     end
+--     local path = vim.fn.input('Path to your *proj file', default_path, 'file')
+--     vim.g['dotnet_last_proj_path'] = path
+--     local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
+--     print('')
+--     print('Cmd to execute: ' .. cmd)
+--     local f = os.execute(cmd)
+--     if f == 0 then
+--         print('\nBuild: ✔️ ')
+--     else
+--         print('\nBuild: ❌ (code: ' .. f .. ')')
+--     end
+-- end
 
-dap.configurations.cs = {
+vim.g.dotnet_get_dll_path = function()
+    local request = function()
+        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/**/bin/**/*.dll', 'file')
+    end
+
+    if vim.g['dotnet_last_dll_path'] == nil then
+        vim.g['dotnet_last_dll_path'] = request()
+    else
+        if vim.fn.confirm('Do you want to change the path to dll?\n' .. vim.g['dotnet_last_dll_path'], '&yes\n&no', 2) == 1 then
+            vim.g['dotnet_last_dll_path'] = request()
+        end
+    end
+
+    return vim.g['dotnet_last_dll_path']
+end
+
+local config = {
 	{
 		type = "coreclr",
-		name = "attach - netcoredbg",
-		request = "attach",
-		processId = function()
-			return require('dap.utils').pick_process({
-				filter = function(proc)
-					return vim.startswith(proc.name, vim.fn.getcwd())
-				end
-			})
+		name = "launch - netcoredbg",
+		request = "launch",
+		program = function()
+				-- if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
+				-- 		vim.g.dotnet_build_project()
+				-- end
+				return vim.g.dotnet_get_dll_path()
 		end,
-		-- processId = function()
-		-- 	return tonumber(vim.fn.input('Process ID: '))
-		-- end,
-		-- console = "integratedTerminal",
-		-- program = function()
-		-- 	return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-		-- 	-- return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '', 'file')
-		-- end,
 	},
+	{
+			type = "coreclr",
+			name = "attach - netcoredbg",
+			request = "attach",
+			processId = function()
+				return require('dap.utils').pick_process({
+					filter = function(proc)
+						return vim.startswith(proc.name, vim.fn.getcwd())
+					end
+				})
+			end,
+		}
 }
+
+dap.configurations.cs = config
+dap.configurations.fsharp = config
 
 require("nvim-dap-virtual-text").setup()
 -- require('dap-go').setup()
