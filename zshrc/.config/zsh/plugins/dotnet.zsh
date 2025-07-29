da71c30a08b41516f0c6d090a3076b00c9e5f115
Albert Gouws <albert@gouws.org>
2025-07-29 15:17:27 +1200

    zshrc: Added dotnet completion

--- zshrc/.config/zsh/plugins/dotnet.zsh
+++ zshrc/.config/zsh/plugins/dotnet.zsh
@@ -7,3 +7,21 @@ dotnet-outdated-update() {
 		xargs -n1 --no-run-if-empty dotnet add package
 }
 
+# FROM: https://learn.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete#zsh
+# zsh parameter completion for the dotnet CLI
+_dotnet_zsh_complete()
+{
+  local completions=("$(dotnet complete "$words")")
+
+  # If the completion list is empty, just continue with filename selection
+  if [ -z "$completions" ]
+  then
+	_arguments '*::arguments: _normal'
+	return
+  fi
+
+  # This is not a variable assignment, don't remove spaces!
+  _values = "${(ps:\n:)completions}"
+}
+
+compdef _dotnet_zsh_complete dotnet
