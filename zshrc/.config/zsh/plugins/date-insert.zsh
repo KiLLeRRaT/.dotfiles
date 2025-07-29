d7b4e38a640f4b979f6b664de38f0a7e4a97d6fb
Albert Gouws <albert@gouws.org>
2025-07-29 17:34:19 +1200

    zshrc(date-insert): Added branch name snippet

--- zshrc/.config/zsh/plugins/date-insert.zsh
+++ zshrc/.config/zsh/plugins/date-insert.zsh
@@ -7,6 +7,7 @@ insert-date() {
 		"$(date -u +'%Y-%m-%dT%H:%M:%SZ')\tISO 8601 (UTC, -u %Y-%m-%dT%H:%M:%SZ)"
 		"$(date +'%A, %d %b')\t\tNZ full (Day, Date Mon, %A, %d %b)"
 		"$(date +'%s')\t\tEpoch seconds (%s)"
+		"feature/$(date +'%Y%m%d')_ag_\t\tBranch name format (feature/YYYYMMDD_ag_, feature/%Y%m%d_ag_)"
 	)
 	date=$(printf '%b\n' "${formats[@]}" | fzf --height=~50 --border --header="Select date format" --query "$1" --select-1 --no-sort | awk -F'\t' '{ print $1 }')
 	LBUFFER+=$date
