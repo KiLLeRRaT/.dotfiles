ebd55c6ed0b46408f17c9c772c2b194d53c3f780
Albert Gouws <albert@gouws.org>
2025-08-20 14:05:34 +1200

    chore: Standardise the install-fzf-from-tar to use /tmp/fzf path

--- install/install-fzf-from-tar.sh
+++ install/install-fzf-from-tar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-pushd ~/Downloads
+mkdir /tmp/fzf
+pushd /tmp/fzf
 
 curl -L -o fzf.tar.gz https://github.com/junegunn/fzf/releases/download/v0.62.0/fzf-0.62.0-linux_amd64.tar.gz
 tar -xzf fzf.tar.gz
