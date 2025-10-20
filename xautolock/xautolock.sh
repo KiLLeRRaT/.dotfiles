7885bbbded3ffd1282ac7b180b39efc873efaca4
Albert Gouws <albert@gouws.org>
2025-10-20 16:56:22 +1300

    xautolock: Added exclusion for a couple of apps

--- xautolock/xautolock.sh
+++ xautolock/xautolock.sh
@@ -3,8 +3,9 @@
 
 # Settings
 declare -a LIST_OF_WINDOW_TITLES_THAT_PREVENT_LOCKING=(
-    "YouTube"
-    "Meet"
+	"YouTube"
+	"Meet"
+	"Microsoft Teams"
 )
 
 # Dependencies
@@ -14,42 +15,42 @@ XPROP=/usr/bin/xprop
 
 # Find active window id
 get_active_id() {
-    $XPROP -root | $AWK '$1 ~ /_NET_ACTIVE_WINDOW/ { print $5 }'
+	$XPROP -root | $AWK '$1 ~ /_NET_ACTIVE_WINDOW/ { print $5 }'
 }
 
 # Determine a window's title text by it's ID
 get_window_title() {
-    # For mplayer or vlc, we might need to check WM_CLASS(STRING), idk.
-    $XPROP -id $1 | $AWK -F '=' '$1 ~ /_NET_WM_NAME\(UTF8_STRING\)/ { print $2 }'
+	# For mplayer or vlc, we might need to check WM_CLASS(STRING), idk.
+	$XPROP -id $1 | $AWK -F '=' '$1 ~ /_NET_WM_NAME\(UTF8_STRING\)/ { print $2 }'
 }
 
 # Determine if a window is fullscreen based on it's ID
 is_fullscreen() {
-    $XPROP -id $1 | $AWK -F '=' '$1 ~ /_NET_WM_STATE\(ATOM\)/ { for (i=2; i<=3; i++) if ($i ~ /FULLSCREEN/) exit 0; exit 1 }'
-    return $?
+	$XPROP -id $1 | $AWK -F '=' '$1 ~ /_NET_WM_STATE\(ATOM\)/ { for (i=2; i<=3; i++) if ($i ~ /FULLSCREEN/) exit 0; exit 1 }'
+	return $?
 }
 
 # Determine if the locker command should run based on which windows are
 # fullscreened.
 should_lock() {
-    id=$(get_active_id)
-    title=$(get_window_title $id)
+	id=$(get_active_id)
+	title=$(get_window_title $id)
 
-    # echo "Active window ID: $id"
-    # echo "Active window title: $title"
+	# echo "Active window ID: $id"
+	# echo "Active window title: $title"
 
-    # if is_fullscreen $id; then
-        for i in "${LIST_OF_WINDOW_TITLES_THAT_PREVENT_LOCKING[@]}"; do
-            # echo "Comparing $i to $title"
-            if [[ $title =~ $i ]]; then
-                return 1
-            fi
-        done
+	# if is_fullscreen $id; then
+		for i in "${LIST_OF_WINDOW_TITLES_THAT_PREVENT_LOCKING[@]}"; do
+			# echo "Comparing $i to $title"
+			if [[ $title =~ $i ]]; then
+				return 1
+			fi
+		done
 
-        return 0
-    # else
-        # return 0
-    # fi
+		return 0
+	# else
+		# return 0
+	# fi
 }
 
 # main()
