e437ae47b2d40d8292743ead218abc007317177e
Albert Gouws <albert@gouws.org>
2025-11-27 12:02:53 +1300

    zsh(npm): Added alias for npm to use op read

--- zshrc/.config/zsh/plugins/npm.zsh
+++ zshrc/.config/zsh/plugins/npm.zsh
@@ -19,3 +19,12 @@ npm-outdated-update() {
 		xargs --no-run-if-empty npm install
 }
 
+if [ -f $HOME/.config/op/npm-env ] && command -v op >/dev/null 2>&1; then
+	# If you don't have the file, perhaps create it, and add the following to it:
+	# NPM_AUTH_TOKEN="op://VAULT/ITEM/FIELD"
+	# Reminder: Make sure the value is base64 encoding, echo -n "NPM_AUTH_TOKEN" | base64
+
+	# alias npm="op run --env-file=$HOME/.config/op/npm-env --no-masking -- env | sort && npm"
+	# alias npm="op run --env-file=$HOME/.config/op/npm-env --no-masking -- npm"
+	alias npm="op run --env-file=$HOME/.config/op/npm-env -- npm --color=always"
+fi
