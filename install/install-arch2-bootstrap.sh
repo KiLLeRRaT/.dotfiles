334280c2043d3f2855b235a7f18384ef2bac311d
Albert Gouws <albert@gouws.org>
2026-02-23 09:54:31 +1300

    install(install-arch2-bootstrap): Cache busting

--- install/install-arch2-bootstrap.sh
+++ install/install-arch2-bootstrap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-curl -H 'Cache-Control: no-cache, no-store' -LOJ https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.10-partition.sh
-curl -H 'Cache-Control: no-cache, no-store' -LOJ https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.20-mount.sh
-curl -H 'Cache-Control: no-cache, no-store' -LOJ https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.30-install.sh
-curl -H 'Cache-Control: no-cache, no-store' -LOJ https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.50-arch-chroot.sh
-curl -H 'Cache-Control: no-cache, no-store' -LOJ https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.90-umount.sh
-curl -H 'Cache-Control: no-cache, no-store' -LOJ https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.variables.sh
-curl -H 'Cache-Control: no-cache, no-store' -LOJ https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/mount-install-env.sh
-curl -H 'Cache-Control: no-cache, no-store' -LOJ https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/umount-install-env.sh
-curl -H 'Cache-Control: no-cache, no-store' -LOJ https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/rescue-arch.sh
+curl -H 'Cache-Control: no-cache, no-store' -LOJ "https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.10-partition.sh?$(date +%s)"
+curl -H 'Cache-Control: no-cache, no-store' -LOJ "https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.20-mount.sh?$(date +%s)"
+curl -H 'Cache-Control: no-cache, no-store' -LOJ "https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.30-install.sh?$(date +%s)"
+curl -H 'Cache-Control: no-cache, no-store' -LOJ "https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.50-arch-chroot.sh?$(date +%s)"
+curl -H 'Cache-Control: no-cache, no-store' -LOJ "https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.90-umount.sh?$(date +%s)"
+curl -H 'Cache-Control: no-cache, no-store' -LOJ "https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/install-arch2.variables.sh?$(date +%s)"
+curl -H 'Cache-Control: no-cache, no-store' -LOJ "https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/mount-install-env.sh?$(date +%s)"
+curl -H 'Cache-Control: no-cache, no-store' -LOJ "https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/umount-install-env.sh?$(date +%s)"
+curl -H 'Cache-Control: no-cache, no-store' -LOJ "https://raw.githubusercontent.com/KiLLeRRaT/.dotfiles/refs/heads/master/install/rescue-arch.sh?$(date +%s)"
 
 chmod u+x *.sh
 echo "Done"
